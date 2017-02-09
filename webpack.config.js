c67c3aa2efc6af1d2578f0cde322524e3ec5f1bb
augustine <ashramwen@gmail.com>
2017-02-09 10:18:06 +0800

    v0.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -12,27 +12,13 @@ fs.readdirSync('node_modules')
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
-var includePaths = [
-  fs.realpathSync(__dirname + '/node_modules/jju'),
-];
-
 module.exports = function () {
   return {
     entry: {
       main: ['./src/main.js', './src/kii.js', 'kii-gateway-agent', 'body-parser', 'net', 'math-uint32-to-binary-string', 'math-float32-from-bits'],
-      mt7688: ['./src/kii_mt7688_v11.js', 'body-parser', 'net', 'math-uint32-to-binary-string', 'math-float32-from-bits'],
-      test: ['./src/test.js', 'kii-gateway-agent'],
-      test2: ['./src/test2.js', './src/kii.js'],
-      vendor: ['kii-gateway-agent']
+      mt7688: ['./src/kii_mt7688_v11.js', 'body-parser', 'net', 'math-uint32-to-binary-string', 'math-float32-from-bits']
     },
     target: 'node',
-    // module: {
-    //   loaders: [{
-    //     test: /\.js?$/,
-    //     include: includePaths,
-    //     loader: 'babel-loader'
-    //   }]
-    // },
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist')
@@ -42,9 +28,6 @@ module.exports = function () {
         from: 'resource',
         to: 'resource'
       }])
-      // new webpack.optimize.CommonsChunkPlugin({
-      //   names: 'vendor'
-      // })
     ],
     externals: {
       'express': 'require("express")',
